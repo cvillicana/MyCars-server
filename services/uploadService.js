be2ac590d68d71405f3d0fde6c337e0b65a57a0c
Unknown <villicana.ca@gmail.com>
2017-09-16 21:34:06 -0500

    added image service

---
+++ services/uploadService.js
@@ -0,0 +1,52 @@
+var path    = require('path'),
+  fs        = require('fs'),
+  AWS       = require('aws-sdk');
+
+AWS.config.loadFromPath('./config/aws.json');
+var s3 = new AWS.S3();
+
+exports.saveFile = function(req, res, next){
+
+  var filename = req.params.filename;
+  var pathFile = path.basename(req.params.filename);
+  pathFile = path.resolve(__dirname, pathFile);
+  var dst = fs.createWriteStream(pathFile);
+  req.pipe(dst);
+
+  dst.on('drain', function(){
+    console.log('drain', new Date());
+    req.resume();
+  });
+
+  req.on('end', function(){
+    uploadFile(pathFile,filename);
+    res.send(200);
+  });
+}
+
+
+function uploadFile(pathFile,filename){
+
+  var stream;
+
+  fs.readFile(pathFile, function read(err, data){
+
+    if (err){
+      console.error("unable to read: ",err);
+    }
+
+    stream = data;
+
+    var params = {
+      Body: stream,
+      Bucket: "vamonos-app",
+      Key: "profile-pictures/" + filename
+    };
+
+    s3.putObject(params, function(err, data){
+      console.log(err, data);
+    });
+
+  });
+
+}
