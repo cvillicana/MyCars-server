45833bc4ad89e0c505d1fd4660c6aba40b1674a0
Unknown <villicana.ca@gmail.com>
2018-04-06 22:23:49 -0500

    set another database

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = function(){
   switch(process.env.NODE_ENV){
     case 'dev':
       return{
-        'url': 'mongodb://ds215019.mlab.com:15019/heroku_hfs9p9rt',
+        'url': 'mongodb://54.144.106.244:27017/carshare-dev',
         options : {
           useMongoClient: true,
           user: 'carsharedba',
